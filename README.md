349f6fcfdc0c7dba8526cccd1d9c48ca877870a1
Marcos <perezmarcos85@gmail.com>
2023-05-06 20:41:30 -0300

    Agrego archivo README, agrego archivo de JavaScript y edito el SRC para llamarlo desde el HTML

---
+++ README.md
@@ -0,0 +1,2 @@
+After downloading / cloning the repository run the command
+npm install bootstrap
---
+++ index.js
@@ -0,0 +1,11 @@
+const $topLeft = document.querySelector(".top-left-panel");
+const $topRight = document.getElementsByClassName(".top-right-panel");
+const $bottomLeft = document.getElementsByClassName(".bottom-left-panel");
+const $bottomRight = document.getElementsByClassName(".bottom-right-panel");
+const $startBtn = document.querySelector("button");
+
+let machineSequence = [];
+let userSequence = [];
+
+
+
